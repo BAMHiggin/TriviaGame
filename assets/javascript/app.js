e76da52fa03602dc2274ff5e28d4ec015b3fcc7a
BAMHiggin <bamhiggin@gmail.com>
2018-11-03 23:20:53 -0500

    cleaning up code, adding readme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ var i = 0;
 var questionTimeout;
 
 //configuration
-var countdownTimer = 60;
+var countdownTimer = 30;
 var gifTimer = 3;
 
 //reset stuff
@@ -68,7 +68,6 @@ $("#btnD").on('click', function () {
 });
 
 
-
 function newGame(idName) {
     i = 0;
     $(idName).parent().hide();
@@ -85,33 +84,23 @@ function newGame(idName) {
 }
 
 
-
-// create timer that begins countdown once start button is clicked
+// timer that begins countdown once start button is clicked
 var intervalId;
 
-// prevents the clock from being sped up unnecessarily
 var clockRunning = false;
 
-// Our stopwatch object
 var stopwatch = {
 
     time: countdownTimer,
-    lap: 1,
 
     reset: function () {
 
         stopwatch.time = countdownTimer;
-        stopwatch.lap = 1;
 
-        // DONE: Change the "display" div to "00:00."
         $("#display").text("00:00");
-
-        // DONE: Empty the "laps" div.
-        $("#laps").text("");
     },
     start: function () {
 
-        // DONE: Use setInterval to start the count here and set the clock to running.
         if (!clockRunning) {
             intervalId = setInterval(stopwatch.count, 1000);
             clockRunning = true;
@@ -119,33 +108,17 @@ var stopwatch = {
     },
     stop: function () {
 
-        // DONE: Use clearInterval to stop the count here and set the clock to not be running.
         clearInterval(intervalId);
         clockRunning = false;
     },
-    recordLap: function () {
 
-        // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-        //       and save the result in a variable.
-        var converted = stopwatch.timeConverter(stopwatch.time);
-
-        // DONE: Add the current lap and time to the "laps" div.
-        $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
-
-        // DONE: Increment lap by 1. Remember, we can't use "this" here.
-        stopwatch.lap++;
-    },
     count: function () {
 
-        // DONE: increment time by 1, remember we cant use "this" here.
         stopwatch.time--;
 
-        // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-        //       and save the result in a variable.
         var converted = stopwatch.timeConverter(stopwatch.time);
         console.log(converted);
 
-        // DONE: Use the variable we just created to show the converted time in the "display" div.
         $("#display").text(converted);
     },
     timeConverter: function (t) {
---
+++ readme.md
@@ -0,0 +1,8 @@
+An Arrested Development Trivia Game
+
+How well do you know the Bluth family?
+
+Press start to being!
+
+
+Find more fun projects of mine at www.bamhigg.in
